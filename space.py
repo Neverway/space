a41ea0a76db99630bc8cb536040d5c42ac7decb6
Lizband <trash.block2@gmail.com>
2019-03-15 22:09:15 -0400

    Add game loop to space.py

--- space.py
+++ space.py
@@ -8,3 +8,20 @@ display_height = 600
 game_display = pygame.display.set_mode((display_width, display_height))
 
 
+clock = pygame.time.Clock()
+
+
+def game_loop():
+    game_exit = False
+
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+
+    clock.tick(30)
+
+
+game_loop()
+pygame.quit()
+quit()
